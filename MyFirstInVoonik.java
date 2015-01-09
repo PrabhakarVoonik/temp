0112da143f20cbfd5c6289a95e91098db9870baa
mpkguru <mpkguru@gmail.com>
2015-01-09 10:57:20 +0530

    this is because of checkig
    bsacjhbadjhbjadhb

---
+++ MyFirstInVoonik.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+public class MyFirstInVoonik
+{
+static
+{
+
+	out.println("Hello! Voonik.com");
+}
+public static void main(String ...args)
+{}
+}
